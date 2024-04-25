9ce43cc4e4d96149fe9f98f12ed9e18ffd15e188
Affanalisher <affanali99@yahoo.com>
2024-04-24 20:30:51 -0500

    Align rrDB to SILVA SEED reference alignment, closes #4

---
+++ code/align_sequences.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# author: Affan
+# inputs: data/raw/rrnDB-5.8_16S_rRNA.fasta
+#         data/references/silva_seed/silva.seed_v138_1.align
+# outputs: data/raw/rrnDB-5.8_16S_rRNA.align
+#
+#
+# we need to include flip=T to make sure all sequences are
+# pointed in the same direction
+#
+
+
+
+code/mothur/mothur '#align.seqs(fasta=data/raw/rrnDB-5.8_16S_rRNA.fasta, reference=data/references/silva_seed/silva.seed_v138_1.align, flip=T)'
